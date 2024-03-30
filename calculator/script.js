345e2a77e8cd8505dec7827020d5d193d74391bc
Nalankumar <Nalankumar@users.noreply.github.com>
2024-03-30 19:37:44 +0530

    Calculator created

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+function display(Number){
+    document.getElementById("result").value += Number;
+}
+function calculate() {
+    var result = document.getElementById("result").value;
+    var x = eval(result);
+    document.getElementById("result").value = x;
+}
+function clearSrcr(){
+    document.getElementById("result").value = "";
+}function display(Number){
+    document.getElementById("result").value += Number;
+}
+function calculate() {
+    var result = document.getElementById("result").value;
+    var x = eval(result);
+    document.getElementById("result").value = x;
+}
+function clearSrcr(){
+    document.getElementById("result").value = "";
+}
